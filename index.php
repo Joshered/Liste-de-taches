fecc161b94a3a55d1feb2995e7345450b63b0593
joshered <charlesyered4@gmail.com>
2025-01-06 23:01:56 +0200

    heder style

--- index.php
+++ index.php
@@ -20,13 +20,14 @@ $taches = $soum -> fetchAll();
         <input type="text" name='tache' placeholder="nouvelle teche" required>
         <input class='btn' type="submit" value='ajouter'>
     </form>
-    <0l type="1">
+    <ol type='1'>
         <?php foreach ($taches as $tache): ?>
             <li>
                 <?php if($tache['tach_accomplies']):?>
                     <tricke><?php echo htmlspecialchars($tache['tache']);?></tricke>
                     <?php else: ?>
                         <?php echo htmlspecialchars($tache['tache']);?>
+                        <?php echo htmlspecialchars($tache['id_tach']);?>
                         <a class='aa' href="taches_termin.php?id_tach=<?php echo $tache['id_tach'];?>">cocher comme termner</a>
                         <?php endif;?>
                         <a class='aaa' href="sup_taches.php?id_tach=<?php echo $tache['id_tach'];?>">supprimer</a>
