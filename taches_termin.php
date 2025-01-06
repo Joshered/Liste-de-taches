a0c48d721541bce19eaa956d6f65617c9f56061a
joshered <charlesyered4@gmail.com>
2025-01-06 22:23:28 +0200

    affichage

--- taches_termin.php
+++ taches_termin.php
@@ -2,10 +2,10 @@
 include 'bdd.php';
 
 if(isset($_GET['id_tach'])){
-    $id = $_GET['id_tach'];
+    $id_tach = $_GET['id_tach'];
 
-    $soum = $pdo->prepare("UPDATE  FROM taches SET tach_accomplies=1 WHERE id_tach = :id");
-    $soum -> execute(['id_tach'=>$id]);
+    $soum = $pdo->prepare("UPDATE taches SET tach_accomplies=1 WHERE id_tach = :id_tach");
+    $soum -> execute(['id_tach'=> $id_tach]);
 
     header('location: index.php');
 }
