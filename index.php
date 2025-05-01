0022938a090ea9bcf08784f2e7f3e770ec3794ec
Joshered <charlesyered4@gmail.com>
2025-05-01 11:57:29 +0200

    container style

--- index.php
+++ index.php
@@ -15,26 +15,28 @@ $taches = $soum -> fetchAll();
     <header class="bg-success text-white">
         <h1 class='h2 ms-2 pb-2' >Liste des taches</h1>
     </header>
+    <section class='container-fluid'>
+        <form action="ajout_tache.php" method="POST">
+            <input type="text" name='tache' placeholder="nouvelle teche" required>
+            <input class='btn' type="submit" value='ajouter'>
+        </form>
+        <ol type='1'>
+            <?php foreach ($taches as $tache): ?>
+                <li>
+                    <?php if($tache['tach_accomplies']):?>
+                        <tricke><?php echo htmlspecialchars($tache['tache']);?></tricke>
+                        <?php else: ?>
+                            <?php echo htmlspecialchars($tache['tache']);?>
+                            <a class='aa' href="taches_termin.php?id_tach=<?php echo $tache['id_tach'];?>">cocher comme termner</a>
+                            <?php endif;?>
+                            <a class='aaa' href="sup_taches.php?id_tach=<?php echo $tache['id_tach'];?>">supprimer</a>
+                            <label for="" class='aab'> <?php echo htmlspecialchars($tache['date_d']);?> </label>
+                            
+                </li>
+            <?php endforeach; ?>
+        </ol>
+    </section>
     
-    <form action="ajout_tache.php" method="POST">
-        <input type="text" name='tache' placeholder="nouvelle teche" required>
-        <input class='btn' type="submit" value='ajouter'>
-    </form>
-    <ol type='1'>
-        <?php foreach ($taches as $tache): ?>
-            <li>
-                <?php if($tache['tach_accomplies']):?>
-                    <tricke><?php echo htmlspecialchars($tache['tache']);?></tricke>
-                    <?php else: ?>
-                        <?php echo htmlspecialchars($tache['tache']);?>
-                        <a class='aa' href="taches_termin.php?id_tach=<?php echo $tache['id_tach'];?>">cocher comme termner</a>
-                        <?php endif;?>
-                        <a class='aaa' href="sup_taches.php?id_tach=<?php echo $tache['id_tach'];?>">supprimer</a>
-                        <label for="" class='aab'> <?php echo htmlspecialchars($tache['date_d']);?> </label>
-                        
-            </li>
-        <?php endforeach; ?>
-    </ol>
     <script src="script.js"></script>
 </body>
 </html>
